681491c0beca77dc5ddf322092a2fa34e516f619
alex <alexandersegerev@gmail.com>
2024-09-23 10:48:17 -0400

    Changed the static Dice 6 to the image of the dice with the random number

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-//Testing
-console.log(randomNumber1);
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomDiceMG = "dice" + randomNumber1 + ".png";
+let randomImgSrc = "images/" + randomDiceMG;
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImgSrc);
+
+let randomDiceMG2 = "dice" + randomNumber2 + ".png";
+let randomImgSrc2 = "images/" + randomDiceMG2;
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImgSrc2);
+
+
+
